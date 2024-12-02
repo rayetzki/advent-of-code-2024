6d7995d8657f7819c402b2aa0d721794f2e8e88d
Illia Raietskyi <IlliaRaietskyi@planstin.com>
2024-12-02 11:56:23 +0200

    [advent-of-code-2024]: Day 1: Historian Hysterian
    day 1 add task desc

---
+++ 01-historian-hysteria/index.mjs
@@ -0,0 +1,52 @@
+import { readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import { REGEX, CHARS } from '../common.mjs';
+
+const fileInput = await readFile(join('./', '01-historian-hysteria', 'input.txt'), { encoding: 'utf-8' });
+
+const leftList = [];
+const rightList = [];
+
+for await (const line of fileInput.split(REGEX.NEWLINE)) {
+  const [left, right] = line.split(CHARS.SPACE.repeat(3));
+  leftList.push(parseInt(left));
+  rightList.push(parseInt(right));
+}
+
+leftList.sort((a, b) => a - b);
+rightList.sort((a, b) => a - b);
+
+console.log("-- PART 1 --");
+
+let total = 0;
+
+for (let i = 0; i < leftList.length; i++) {
+  total += Math.abs(leftList[i] - rightList[i]);
+}
+
+console.log("Result: ", total);
+
+console.log("-- PART 2 --");
+
+const similarityScores = new Map();
+let totalSimilarityScore = 0;
+
+for (const left of leftList) {
+  if (similarityScores.has(left)) {
+    totalSimilarityScore += left * similarityScores.get(left);
+    continue;
+  }
+  
+  let appearTimes = 0;
+
+  for (const right of rightList) {
+    if (left === right) {
+      appearTimes++;
+    }
+  }
+
+  totalSimilarityScore += left * appearTimes;
+  similarityScores.set(left, appearTimes);
+}
+
+console.log("Result: ", totalSimilarityScore);
---
+++ common.mjs
@@ -0,0 +1,12 @@
+export const REGEX = {
+  ALPHABET: /[a-z]/i, 
+  SPACE: /\s/g, 
+  NEWLINE: /\n/g,
+  NUMS: /[0-9]+/g,
+  DOUBLE_NEWLINE: /\n\n/,
+  SEPARATOR: /,|-|:/,
+};
+
+export const CHARS = {
+  SPACE: ' ',
+};
