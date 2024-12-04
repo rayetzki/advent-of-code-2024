a1bc22b1024eb6a204dda2a5001589b348193378
Illia Raietskyi <IlliaRaietskyi@planstin.com>
2024-12-04 07:43:01 +0200

    [advent-of-code-2024]: Day 2: Red Nosed Reports

---
+++ 02-red-nosed-reports/index.mjs
@@ -0,0 +1,55 @@
+import { readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import { REGEX, CHARS } from '../common.mjs';
+
+const inputFilePath = join('./', import.meta.dirname, 'input.txt');
+const fileInput = await readFile(inputFilePath, { encoding: 'utf-8' });
+
+const reports = fileInput
+  .split(REGEX.NEWLINE)
+  .map((line) => line.split(CHARS.SPACE).map(Number));
+
+const checkReport = (report) => {
+  const difference = report.reduce((acc, _, levelIndex) => {
+    if (levelIndex === report.length - 1) return acc;
+    acc.push(report[levelIndex] - report[levelIndex + 1]);
+    return acc;
+  }, []);
+
+  return difference.every((level) => (
+    Math.sign(level) === Math.sign(difference.at(0)) &&
+    Math.abs(level) >= 1 &&
+    Math.abs(level) <= 3
+  ));
+}
+  
+console.log('-- PART 1 --');
+
+const totalSafeReports = reports.reduce((acc, report) => {
+  const isSafe = checkReport(report);
+  return isSafe ? acc + 1 : acc;
+}, 0);
+
+console.log('Result:', totalSafeReports);
+
+console.log('-- PART 2 --');
+
+const totalSafeReportsWithDamping = reports.reduce((acc, report) => {
+  const isAlreadySafe = checkReport(report);
+  if (isAlreadySafe) return acc + 1;
+
+  for (let i = 0; i < report.length; i++) {
+    const isSafeDamped = checkReport([
+      ...report.slice(0, i),
+      ...report.slice(i + 1),
+    ]);
+
+    if (isSafeDamped) {
+      return acc + 1
+    };
+  }
+
+  return acc;
+}, 0);
+
+console.log('Result:', totalSafeReportsWithDamping);
